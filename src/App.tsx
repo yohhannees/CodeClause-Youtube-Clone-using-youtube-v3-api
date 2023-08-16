9e948d7eaf36fdc3deb2230020e216945421b527
Yohannees <yohannesgetachewerieso@gmail.com>
2023-08-16 15:48:24 +0300

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,17 @@ import VideoList from "./component/VideoList";
 import NavBar from "./component/NavBar";
 import Header from "./component/Header";
 import Sidebar from "./component/Sidebar";
-
 function App() {
   return (
     <div className="container mx-auto p-4">
-      <Header />
+      {/* <Header />
       <div className="my-16 ">
        
         <NavBar />
       </div>
 
-      <VideoList />
-      {/* <Sidebar /> */}
+      <VideoList /> */}
+      <Sidebar />
     </div>
   );
 }
