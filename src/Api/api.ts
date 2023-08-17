4b083d1beab724b7a3218b5d7d22fe50d549dea5
Yohannes Getachew <105593295+yohhannees@users.noreply.github.com>
2023-08-17 17:41:12 +0300

    updated

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-// const API_KEY = 'AIzaSyBN4vbtPGWuYDhgCUvebdn94QXp0GTZ9PU';
-const API_KEY='AIzaSyBHZ_S_gmXpiAnAH-mibx9f4EXX6cJqCEY';
 
+const API_KEY='Place Api key here';
 const axiosInstance = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
   params: {
