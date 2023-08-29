62daa3355e45c2f5a05fa38d2ec0c09681a43edd
Yohannes Getachew <105593295+yohhannees@users.noreply.github.com>
2023-08-29 09:51:16 +0300

    Update api.ts

--- src/Api/api.ts
+++ src/Api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_KEY='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';
+const API_KEY='AIzaSyBN4vbtPGWuYDhgCUvebdn94QXp0GTZ9PU';
 const axiosInstance = axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3/',
   params: {
